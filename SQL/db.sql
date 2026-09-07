922e0763356d3998a919f44a11b64cb8f39a00af
Rishit Singh <rishitsingh.286@gmail.com>
2026-09-07 23:13:17 +0530

    Add SQL script to create database and employee tables

---
+++ SQL/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE SQLTutorials;
+USE SQLTutorials;
+
+CREATE TABLE EmployeeDemographics(
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Age INT,
+    Gender VARCHAR(50)
+)
+
+CREATE TABLE EmployeeSalary(
+    EmployeeID INT,
+    JobTitile VARCHAR(50),
+    Salary INT
+)
+
